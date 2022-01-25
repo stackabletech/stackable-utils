2e815c15cdc2af7b99b22e9c83538536db359d1d
Sönke Liebau <soenke.liebau@stackable.de>
2022-01-25 15:50:10 +0100

    Update stackable-bcrypt/src/main.rs
    Co-authored-by: Teo Klestrup Röijezon <teo.roijezon@stackable.de>

--- stackable-bcrypt/src/main.rs
+++ stackable-bcrypt/src/main.rs
@@ -7,7 +7,7 @@ use std::process::exit;
     long_about = "A tiny command line tool to hash a given string using the bcrypt algorithm.\
         Default cost used is 10, but can be configured. \
         The output is only the hash, so that this can be used for automation purposes without any parsing.",
-    author = "Stackable GmbH - info@stackable.de"
+    author
 )]
 struct Opts {
     #[clap(short, long, default_value = "10")]
