1ced155581290f187ec2c1191dfe5840ef7bc0c7
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2022-12-01 13:55:39 +0100

    wip push changes

--- release/create-release-branch.sh
+++ release/create-release-branch.sh
@@ -17,6 +17,7 @@ RELEASE_TAG="${RELEASE_TAG%\"}"
 RELEASE_TAG="${RELEASE_TAG#\"}"
 
 RELEASE_BRANCH="release-$RELEASE_TAG"
+echo ${RELEASE_BRANCH}
 
 ensure_release_branch() {
   local STATUS=$(git status -s | grep -v '??')
@@ -27,7 +28,6 @@ ensure_release_branch() {
   fi
 
   git switch -c ${RELEASE_BRANCH} ${BASE_BRANCH}
-  #git push -u ${REPOSITORY} ${RELEASE_BRANCH}
 }
 
 update_antora() {
@@ -39,24 +39,24 @@ update_antora() {
 main() {
   # for each callout:
   # - create new release branch based on the tag argument
-  # - replace "nightly" with $RELEASE_TAG in antora.yaml
+  # - replace "nightly" with "$RELEASE_TAG" (i.e. with qoutes) in antora.yaml
   # - set pre-release=false in antora.yaml
-  # - set version to $RELEASE_TAG in docs_templating.sh
-
-  local PUSH=${2:-false}
+  # - set version to $RELEASE_TAG (without quotes) in docs_templating.sh
 
   ensure_release_branch
   update_antora
 
-  #git commit -am "release $RELEASE_TAG"
-  #git tag -a $RELEASE_TAG -m "release $RELEASE_TAG"
+  git commit -am "release $RELEASE_TAG"
+  git tag -a $RELEASE_TAG -m "release $RELEASE_TAG"
 
-  #if [ "$PUSH" = "true" ]; then
-  #  git push ${REPOSITORY} ${RELEASE_BRANCH}
-  #  # git push --tags
-  #  maybe_create_github_pr $MESSAGE
-  #  git switch main
-  #fi
+  if [ "$#" -eq  "2" ]; then
+    if [[ $2 == '-d' ]]; then
+      echo "Pushing changes..."
+      git push ${REPOSITORY} ${RELEASE_BRANCH}
+      git push --tags
+      git switch main
+    fi
+   fi
 }
 
 main $@
