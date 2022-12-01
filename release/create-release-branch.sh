749651c23df3bb054fdf22b5160029b23b460d5f
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2022-12-01 13:11:27 +0100

    wip create release branch

--- release/create-release-branch.sh
+++ release/create-release-branch.sh
@@ -10,12 +10,13 @@ set -x
 
 BASE_BRANCH="main"
 REPOSITORY="origin"
-NOW_UTC=$(date -u '+%Y%m%d%H%M%S')
 
 RELEASE_TAG=$1
-RELEASE_BRANCH="release-$RELEASE_TAG"
+# remove leading and trailing quotes
+RELEASE_TAG="${RELEASE_TAG%\"}"
+RELEASE_TAG="${RELEASE_TAG#\"}"
 
-CARGO_VERSION=$(dirname -- "${BASH_SOURCE[0]}")/cargo-version.py
+RELEASE_BRANCH="release-$RELEASE_TAG"
 
 ensure_release_branch() {
   local STATUS=$(git status -s | grep -v '??')
@@ -26,70 +27,36 @@ ensure_release_branch() {
   fi
 
   git switch -c ${RELEASE_BRANCH} ${BASE_BRANCH}
-  git push -u ${REPOSITORY} ${RELEASE_BRANCH}
+  #git push -u ${REPOSITORY} ${RELEASE_BRANCH}
 }
 
-maybe_create_github_pr() {
-  local MESSAGE=$@
-  GH_COMMAND=$(which gh)
-  if [ "$GH_COMMAND" != "" ]; then
-    gh pr create --base $BASE_BRANCH --head $RELEASE_BRANCH --title "$MESSAGE" --body "$MESSAGE"
-  fi
-}
-
-update_changelog() {
-  local RELEASE_VERSION=$1;
-
-  TODAY=$(date +'%Y-%m-%d')
-
-  sed -i "s/^.*unreleased.*/## [Unreleased]\n\n## [$RELEASE_VERSION] - $TODAY/I" CHANGELOG.md
+update_antora() {
+  yq -i ".version = \"${RELEASE_TAG}\"" docs/antora.yml
+  yq -i '.prerelease = false' docs/antora.yml
+  yq -i ".versions[] = ${RELEASE_TAG}" docs/templating_vars.yaml
 }
 
 main() {
+  # for each callout:
+  # - create new release branch based on the tag argument
+  # - replace "nightly" with $RELEASE_TAG in antora.yaml
+  # - set pre-release=false in antora.yaml
+  # - set version to $RELEASE_TAG in docs_templating.sh
 
-  local NEXT_LEVEL=${1:-"release"}
-  local PUSH=${2:-true}
+  local PUSH=${2:-false}
 
   ensure_release_branch
+  update_antora
 
-  if [ "$NEXT_LEVEL" == "release" ]; then
-    #
-    # Release
-    #
-    $CARGO_VERSION --release
-    cargo update --workspace
-    make regenerate-charts
-    local RELEASE_VERSION=$($CARGO_VERSION --show)
-    local DOCS_VERSION=$(echo "${RELEASE_VERSION}" | sed 's/^\([0-9]\+\.[0-9]\+\)\..*$/\1/')
+  #git commit -am "release $RELEASE_TAG"
+  #git tag -a $RELEASE_TAG -m "release $RELEASE_TAG"
 
-    update_changelog $RELEASE_VERSION
-
-    MESSAGE="release $RELEASE_VERSION"
-    git commit -am "release $RELEASE_VERSION"
-    git tag -a $RELEASE_VERSION -m "release $RELEASE_VERSION"
-    # We don't want to tag the docs in all the cases, as some manual steps might be needed (e.g. updating getting started guides)
-    # git tag -a docs/$DOCS_VERSION -m "docs $DOCS_VERSION"
-
-  else
-    #
-    # Development
-    #
-    $CARGO_VERSION --next ${NEXT_LEVEL}
-    cargo update --workspace
-    make regenerate-charts
-    local RELEASE_VERSION=$($CARGO_VERSION --show)
-
-    MESSAGE="bump version $RELEASE_VERSION"
-    git commit -am "bump version $RELEASE_VERSION"
-
-  fi
-
-  if [ "$PUSH" = "true" ]; then
-    git push ${REPOSITORY} ${RELEASE_BRANCH}
-    # git push --tags
-    maybe_create_github_pr $MESSAGE
-    git switch main
-  fi
+  #if [ "$PUSH" = "true" ]; then
+  #  git push ${REPOSITORY} ${RELEASE_BRANCH}
+  #  # git push --tags
+  #  maybe_create_github_pr $MESSAGE
+  #  git switch main
+  #fi
 }
 
 main $@
