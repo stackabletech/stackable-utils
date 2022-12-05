5b5ab96df8c549c637242c1f85daf51c24868c5a
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2022-12-05 17:55:52 +0100

    added some explanation

--- release/create-release-branch.sh
+++ release/create-release-branch.sh
@@ -1,32 +1,47 @@
 #!/usr/bin/env bash
-#
+#----------------------------------------------------------------------------------------------------
 # Usage: create-release-branch.sh <release-tag> [-p]
 #
 # <release-tag> : e.g. "23.1"
 # [-p]: push changes (otherwise is effectively a dry run)
+# This script requires https://github.com/mikefarah/yq (not to be confused with https://github.com/kislyuk/yq)
 #
-set -e
+# What this script does:
+# - checks that the release argument is valid (e.g. semver-compatible, just major/minor levels)
+# - strips this argument of any leading or trailing quote marks
+# - for docker images:
+#   - creates a new folder in a temporary folder and clones the images repository
+#   - creates a new branch (and pushes it if the push argument is provided)
+# - for operators:
+#   - iterates over a list of operator repository names (operators_to_release.txt), and for each one:
+#   - creates a new folder in a temporary folder and clones the operator repository
+#   - creates a new branch
+#   - creates a one-off commit in the branch (i.e. the changes are valid for the branch lifetime)
+#   - pushes the commit if the push argument is provided
+#----------------------------------------------------------------------------------------------------
+set -euo pipefail
 set -x
 
 BASE_BRANCH="main"
 REPOSITORY="origin"
-
 RELEASE=$1
+#----------------------------------------------------------------------------------------------------
 # tags should be semver-compatible e.g. 23.1 and not 23.01
 # this is needed for cargo commands to work properly: although it is not strictly needed
 # for the name of the release branch, the branch naming will be consistent with the cargo versioning.
+#----------------------------------------------------------------------------------------------------
 RELEASE_REGEX="^[0-9][0-9]\.([1-9]|[1][0-2])$"
+#-----------------------------------------------------------
 # remove leading and trailing quotes
+#-----------------------------------------------------------
 RELEASE="${RELEASE%\"}"
 RELEASE="${RELEASE#\"}"
-
 RELEASE_BRANCH="release-$RELEASE"
 echo "Working release branch: ${RELEASE_BRANCH}"
 
 #DOCKER_IMAGES_REPO="docker-images"
 DOCKER_IMAGES_REPO="test-platform-release-images"
 TEMP_RELEASE_FOLDER="/tmp/stackable-$RELEASE_BRANCH"
-
 INITIAL_DIR="$PWD"
 
 clone_repos() {
@@ -70,15 +85,19 @@ update_antora() {
 }
 
 main() {
+  #-----------------------------------------------------------
   # check if tag argument provided
+  #-----------------------------------------------------------
   if [ -z ${RELEASE+x} ]; then
     echo "Usage: create-release-branch.sh <tag>"
-    exit -1
+    exit 1
   fi
+  #-----------------------------------------------------------
   # check if argument matches our tag regex
+  #-----------------------------------------------------------
   if [[ ! $RELEASE =~ $RELEASE_REGEX ]]; then
     echo "Provided tag [$RELEASE] does not match the required tag regex pattern [$RELEASE_REGEX]"
-    exit -1
+    exit 1
   fi
 
   echo "Cloning docker images and operators to [$TEMP_RELEASE_FOLDER]"
--- release/create-release-tag.sh
+++ release/create-release-tag.sh
@@ -1,28 +1,50 @@
 #!/usr/bin/env bash
-#
-# Usage: create-release-tag.sh <release-tag> [-d]
+#----------------------------------------------------------------------------------------------------
+# Usage: create-release-tag.sh <release-tag> [-p]
 #
 # <release-tag> : e.g. "23.1.42"
-# [-d]: dry run
+# [-p]: push changes (otherwise is effectively a dry run)
+# This script requires https://github.com/mikefarah/yq (not to be confused with https://github.com/kislyuk/yq)
 #
 # What this script does:
-# - Adapt versions in all cargo.toml to <release-tag> argument
-# - Adapt versions in all helm charts to <release-tag> argument
-# - Bump the changelog
-# - Adapt versions in CRDs in the getting started section
-set -e
-#set -x
+# - checks that the release argument is valid (e.g. semver-compatible, major/minor/patch levels)
+# - strips this argument of any leading or trailing quote marks and derives the corresponding branch name
+# - for docker images:
+#   - creates a new folder in a temporary folder and clones the images repository
+#   - switches to the release branch
+#   - tags the branch and pushes it if the push argument is provided
+# - for operators:
+#   - iterates over a list of operator repository names (operators_to_release.txt), and for each one:
+#   - creates a new folder in a temporary folder and clones the operator repository
+#   - switches to the release branch
+#   - makes the following changes:
+#     - adapts the versions in all cargo.toml to <release-tag> argument
+#     - adapts the versions in all helm charts to <release-tag> argument
+#     - updates the workspace
+#     - rebuilds the helm charts
+#     - bumps the changelog
+#     - adapts the versions in CRDs in the getting started section to <release-tag> argument
+#     - runs the templating script to propogate changes to script files
+#   - creates a commit in the branch (i.e. the changes are valid for the branch lifetime)
+#   - pushes the commit if the push argument is provided
+#----------------------------------------------------------------------------------------------------
+set -euo pipefail
+set -x
 
 RELEASE_TAG=$1
-# tags should be semver-compatible e.g. 23.1.1 and not 23.01.1
+#-----------------------------------------------------------
+# tags should be semver-compatible e.g. 23.1.1 not 23.01.1
 # this is needed for cargo commands to work properly
+#-----------------------------------------------------------
 TAG_REGEX="^[0-9][0-9]\.([1-9]|[1][0-2])\.[0-9]+$"
-
+#-----------------------------------------------------------
 # remove leading and trailing quotes
+#-----------------------------------------------------------
 RELEASE_TAG="${RELEASE_TAG%\"}"
 RELEASE_TAG="${RELEASE_TAG#\"}"
-
-# for a tag of e.g. 23.1.1 the release branch (already created) will be 23.1
+#----------------------------------------------------------------------------------------------------
+# for a tag of e.g. 23.1.1, the release branch (already created) will be 23.1
+#----------------------------------------------------------------------------------------------------
 RELEASE="$(cut -d'.' -f1,2 <<<"$RELEASE_TAG")"
 RELEASE_BRANCH="release-$RELEASE"
 echo "Working release branch: ${RELEASE_BRANCH}"
@@ -37,10 +59,12 @@ clone_and_tag_repos() {
   mkdir -p "$TEMP_RELEASE_FOLDER" && cd "$TEMP_RELEASE_FOLDER"
   git clone "git@github.com:stackabletech/${DOCKER_IMAGES_REPO}.git"
   cd "$DOCKER_IMAGES_REPO"
+  #-----------------------------------------------------------
   # the release branch should already exist
+  #-----------------------------------------------------------
   git pull && git switch "$RELEASE_BRANCH"
   #git tag "$RELEASE_TAG"
-  #push_branch "$@"
+  push_branch "$@"
 
   cd "$TEMP_RELEASE_FOLDER"
 
@@ -57,28 +81,27 @@ clone_and_tag_repos() {
     make regenerate-charts
 
     update_code "$TEMP_RELEASE_FOLDER/${operator}"
+    #-----------------------------------------------------------
     # ensure .j2 changes are resolved
+    #-----------------------------------------------------------
     "$TEMP_RELEASE_FOLDER/${operator}"/scripts/docs_templating.sh
-
+    #-----------------------------------------------------------
     # inserts a single line with tag and date
+    #-----------------------------------------------------------
     update_changelog "$TEMP_RELEASE_FOLDER/${operator}"
 
-    #git commit -am "release $RELEASE_TAG"
+    git commit -am "release $RELEASE_TAG"
     #git tag "$RELEASE_TAG"
-    #push_branch "$@"
+    push_branch "$@"
   done < "$INITIAL_DIR"/release/operators_to_release.txt
 }
 
 update_code() {
-  # these 3 instructions would have been executed when the branch was created:
-  #yq -i ".version = \"${RELEASE_TAG}\"" "$1/docs/antora.yml"
-  #yq -i ".prerelease = false" "$1docs/antora.yml"
-  #yq -i ".versions[] = \"${RELEASE_TAG}\"" "$1docs/templating_vars.yaml"
-
   yq -i ".helm.repo_name |= sub(\"stackable-dev\", \"stackable-stable\")" "$1/docs/templating_vars.yaml"
   yq -i ".helm.repo_url |= sub(\"helm-dev\", \"helm-stable\")" "$1/docs/templating_vars.yaml"
-
+  #-----------------------------------------------------------
   # Replace spec.version for *.stackable.tech documents
+  #-----------------------------------------------------------
   for file in "$1"/docs/modules/getting_started/examples/code/*.yaml; do
     if yq ".apiVersion | select(. | contains(\"stackable.tech\")) | parent | .spec | select (. | has(\"version\"))" "$file" | grep version
     then
@@ -105,15 +128,19 @@ update_changelog() {
 }
 
 main() {
+  #-----------------------------------------------------------
   # check if tag argument provided
+  #-----------------------------------------------------------
   if [ -z ${RELEASE_TAG+x} ]; then
     echo "Usage: create-release-tag.sh <tag>"
-    exit -1
+    exit 1
   fi
+  #-----------------------------------------------------------
   # check if argument matches our tag regex
+  #-----------------------------------------------------------
   if [[ ! $RELEASE_TAG =~ $TAG_REGEX ]]; then
     echo "Provided tag [$RELEASE_TAG] does not match the required tag regex pattern [$TAG_REGEX]"
-    exit -1
+    exit 1
   fi
 
   echo "Cloning docker images and operators to [$TEMP_RELEASE_FOLDER]"
