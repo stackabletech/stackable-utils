b2b379db3f818d57c87bdb385c9e4eaf8bb73c45
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2023-02-17 13:44:10 +0100

    improved script

--- olm/build-catalog.sh
+++ olm/build-catalog.sh
@@ -1,15 +1,31 @@
 #!/usr/bin/env bash
+# Usage:
+#   ./olm/build-catalog.sh <release as x.y.z> [-n] [-s]
+#   -r <release>: the release number (mandatory). This must be a semver-compatible value to patch-level e.g. 23.1.0.
+#   -n: create namespace stackable-operators (optional, default is "false").
+#   -s: deploy subscriptions (optional, default is "false").
 
 set -euo pipefail
 set -x
 
-main() {
-  VERSION="$1";
 
-  pushd olm
-  echo "Deploy custom scc..."
-  kubectl apply -f scc.yaml
+parse_inputs() {
+  VERSION=""
+  NAMESPACE=false
+  SUBSCRIPTIONS=false
 
+  while [[ "$#" -gt 0 ]]; do
+      case $1 in
+          -r|--release) VERSION="$2"; shift ;;
+          -n|--namespace) NAMESPACE=true ;;
+          -s|--subscriptions) SUBSCRIPTIONS=true ;;
+          *) echo "Unknown parameter passed: $1"; exit 1 ;;
+      esac
+      shift
+  done
+}
+
+setup() {
   if [ -d "catalog" ]; then
     rm -rf catalog
   fi
@@ -17,10 +33,22 @@ main() {
     rm -rf subscriptions
   fi
 
-  # catalog (just creates dockerfile with copy command)
   mkdir -p catalog
   mkdir -p subscriptions
   rm -f catalog.Dockerfile
+}
+
+prerequisites() {
+  echo "Deploy custom scc..."
+  kubectl apply -f scc.yaml
+
+  if $NAMESPACE; then
+    echo "Creating namespace..."
+    kubectl apply -f namespace.yaml
+  fi
+}
+
+catalog() {
   opm generate dockerfile catalog
 
   # iterate over operators
@@ -50,11 +78,9 @@ main() {
   echo "Build and push catalog for all operators..."
   docker build . -f catalog.Dockerfile -t "docker.stackable.tech/stackable/stackable-operators-catalog:${VERSION}"
   docker push "docker.stackable.tech/stackable/stackable-operators-catalog:${VERSION}"
+}
 
-  # install catalog/group for all operators
-  #kubectl apply -f catalog-source.yaml
-  #kubectl apply -f operator-group.yaml
-
+subscriptions() {
   # iterate over operator list to deploy
   while IFS="" read -r operator || [ -n "$operator" ]
   do
@@ -73,8 +99,30 @@ main() {
     echo "  sourceNamespace: stackable-operators"
     echo "  installPlanApproval: Automatic"
     } >> "subscriptions/$operator-subscription.yaml"
-    # kubectl apply -f "subscriptions/$operator-subscription.yaml"
+    if $SUBSCRIPTIONS; then
+      kubectl apply -f "subscriptions/$operator-subscription.yaml"
+    fi
   done < <(yq '... comments="" | .operators[] ' config.yaml)
+}
+
+main() {
+  parse_inputs "$@"
+  if [ -z "${VERSION}" ]; then
+    echo "Usage: build-catalog.sh -r <release>"
+    exit 1
+  fi
+
+  pushd olm
+
+  setup
+  prerequisites
+  catalog
+
+  # install catalog/group for all operators
+  kubectl apply -f catalog-source.yaml
+  kubectl apply -f operator-group.yaml
+
+  subscriptions
 
   popd
   echo "Deployment successful!"
