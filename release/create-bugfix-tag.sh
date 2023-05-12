e48889e4c82ec1c3b26132fc1292b45d1a4cec7f
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2023-05-12 14:34:50 +0200

    bugfix/patch release script (#44)

--- release/create-bugfix-tag.sh
+++ release/create-bugfix-tag.sh
@@ -12,15 +12,8 @@ TAG_REGEX="^[0-9][0-9]\.([1-9]|[1][0-2])\.[0-9]+$"
 REPOSITORY="origin"
 
 tag_products() {
-  # assume that the branch exists and has either been pushed or has been created locally
   cd "$TEMP_RELEASE_FOLDER/$DOCKER_IMAGES_REPO"
-  #-----------------------------------------------------------
-  # the release branch should already exist
-  #-----------------------------------------------------------
   git switch "$RELEASE_BRANCH"
-  update_product_images_changelogs
-
-  git commit -am "release $RELEASE_TAG"
   git tag "$RELEASE_TAG"
   push_branch
 }
@@ -35,23 +28,7 @@ tag_operators() {
     if [ -f .gitmodules ]; then
       git submodule update --recursive --init
     fi
-
-    CARGO_VERSION="$INITIAL_DIR"/release/cargo-version.py
-    $CARGO_VERSION --set "$RELEASE_TAG"
-    cargo update --workspace
-    make regenerate-charts
-
-    update_code "$TEMP_RELEASE_FOLDER/${operator}"
-    #-----------------------------------------------------------
-    # ensure .j2 changes are resolved
-    #-----------------------------------------------------------
-    "$TEMP_RELEASE_FOLDER/${operator}"/scripts/docs_templating.sh
-    #-----------------------------------------------------------
-    # inserts a single line with tag and date
-    #-----------------------------------------------------------
-    update_changelog "$TEMP_RELEASE_FOLDER/${operator}"
-
-    git commit -am "release $RELEASE_TAG"
+    
     git tag "$RELEASE_TAG"
     push_branch
   done < <(yq '... comments="" | .operators[] ' "$INITIAL_DIR"/release/config.yaml)
@@ -67,15 +44,15 @@ tag_repos() {
 }
 
 check_products() {
-  if [ ! -d "$TEMP_RELEASE_FOLDER/$DOCKER_IMAGES_REPO" ]; then
-    echo "Expected folder is missing: $TEMP_RELEASE_FOLDER/$DOCKER_IMAGES_REPO"
-    exit 1
-  fi
+  #-------------------------------------------------------------
+  # $TEMP_RELEASE_FOLDER has been created in the calling routine
+  #-------------------------------------------------------------
+  git clone "git@github.com:stackabletech/${DOCKER_IMAGES_REPO}.git" "$TEMP_RELEASE_FOLDER/$DOCKER_IMAGES_REPO"
   cd "$TEMP_RELEASE_FOLDER/$DOCKER_IMAGES_REPO"
-  #-----------------------------------------------------------
-  # the up-to-date release branch has already been pulled
-  # N.B. look for exact match (no -rcXXX)
-  #-----------------------------------------------------------
+  #---------------------------------------
+  # switch to the release branch
+  # N.B. look for exact match
+  #---------------------------------------
   BRANCH_EXISTS=$(git branch -r | grep -E "$RELEASE_BRANCH$")
 
   if [ -z "${BRANCH_EXISTS}" ]; then
@@ -83,8 +60,11 @@ check_products() {
     exit 1
   fi
 
+  git switch "${RELEASE_BRANCH}"
   git fetch --tags
-
+  #---------------------------------------
+  # check tags: N.B. look for exact match
+  #---------------------------------------
   TAG_EXISTS=$(git tag -l | grep -E "$RELEASE_TAG$")
   if [ -n "$TAG_EXISTS" ]; then
     echo "Tag $RELEASE_TAG already exists in $DOCKER_IMAGES_REPO"
@@ -93,19 +73,23 @@ check_products() {
 }
 
 check_operators() {
+  #-------------------------------------------------------------
+  # $TEMP_RELEASE_FOLDER has been created in the calling routine
+  #-------------------------------------------------------------
   while IFS="" read -r operator || [ -n "$operator" ]
   do
     echo "Operator: $operator"
-    if [ ! -d "$TEMP_RELEASE_FOLDER/${operator}" ]; then
-      echo "Expected folder is missing: $TEMP_RELEASE_FOLDER/${operator}"
-      exit 1
-    fi
+    git clone "git@github.com:stackabletech/${operator}.git" "$TEMP_RELEASE_FOLDER/${operator}"
     cd "$TEMP_RELEASE_FOLDER/${operator}"
+
     BRANCH_EXISTS=$(git branch -r | grep -E "$RELEASE_BRANCH$")
+
     if [ -z "${BRANCH_EXISTS}" ]; then
       echo "Expected release branch is missing: ${operator}/$RELEASE_BRANCH"
       exit 1
     fi
+    git switch "${RELEASE_BRANCH}"
+
     git fetch --tags
     TAG_EXISTS=$(git tag -l | grep -E "$RELEASE_TAG$")
     if [ -n "${TAG_EXISTS}" ]; then
@@ -124,47 +108,6 @@ checks() {
   fi
 }
 
-update_code() {
-  echo "Updating antora docs for $1"
-  # antora version should be major.minor, not patch level
-  yq -i ".version = \"${RELEASE}\"" "$1/docs/antora.yml"
-  yq -i '.prerelease = false' "$1/docs/antora.yml"
-
-  # Not all operators have a getting started guide
-  # that's why we verify if templating_vars.yaml exists.
-  if [ -f "$1/docs/templating_vars.yaml" ]; then
-    yq -i "(.versions.[] | select(. == \"*dev\")) |= \"${RELEASE_TAG}\"" "$1/docs/templating_vars.yaml"
-    yq -i ".helm.repo_name |= sub(\"stackable-dev\", \"stackable-stable\")" "$1/docs/templating_vars.yaml"
-    yq -i ".helm.repo_url |= sub(\"helm-dev\", \"helm-stable\")" "$1/docs/templating_vars.yaml"
-  fi
-
-  #--------------------------------------------------------------------------
-  # Replace .spec.image.stackableVersion for getting-started examples.
-  # N.B. yaml files should contain a single document.
-  #--------------------------------------------------------------------------
-  if [ -d "$1/docs/modules/getting_started/examples/code" ]; then
-    for file in $(find "$1/docs/modules/getting_started/examples/code" -name "*.yaml"); do
-      yq -i "(.spec | select(has(\"image\")).image | (select(has(\"stackableVersion\")).stackableVersion)) = \"${RELEASE_TAG}\"" "$file"
-    done
-  fi
-
-    #--------------------------------------------------------------------------
-    # Replace .spec.image.stackableVersion for kuttl tests.
-    # Use sed as yq does not process .j2 file syntax properly.
-    #--------------------------------------------------------------------------
-    # TODO old product images won't be tested any longer
-    if [ -f "$1/tests/test-definition.yaml" ]; then
-      # e.g. 2.2.4-stackable0.5.0 -> 2.2.4-stackable23.1
-      sed -i "s/-stackable.*/-stackable${RELEASE_TAG}/" "$1/tests/test-definition.yaml"
-    fi
-
-    #--------------------------------------------------------------------------
-    # Replace "nightly" link so the documentation refers to the current version
-    #--------------------------------------------------------------------------
-    for file in $(find "$1/docs" -name "*.adoc"); do
-      sed -i "s/nightly@home/home/g" "$file"
-    done
-}
 
 push_branch() {
   if $PUSH; then
@@ -182,16 +125,6 @@ cleanup() {
     echo "Cleaning up..."
     rm -rf "$TEMP_RELEASE_FOLDER"
   fi
-}
-
-update_changelog() {
-  TODAY=$(date +'%Y-%m-%d')
-  sed -i "s/^.*unreleased.*/## [Unreleased]\n\n## [$RELEASE_TAG] - $TODAY/I" "$1"/CHANGELOG.md
-}
-
-update_product_images_changelogs() {
-  TODAY=$(date +'%Y-%m-%d')
-  sed -i "s/^.*unreleased.*/## [Unreleased]\n\n## [$RELEASE_TAG] - $TODAY/I" ./**/CHANGELOG.md
 }
 
 parse_inputs() {
@@ -245,13 +178,20 @@ main() {
     exit 1
   fi
 
+  if [ -d "$TEMP_RELEASE_FOLDER" ]; then
+    echo "Folder already exists, please delete it!: $TEMP_RELEASE_FOLDER"
+    exit 1
+  fi
+
+  echo "Creating folder for cloning docker images and operators: [$TEMP_RELEASE_FOLDER]"
+  mkdir -p "$TEMP_RELEASE_FOLDER"
+
   # sanity checks before we start: folder, branches etc.
   # deactivate -e so that piped commands can be used
   set +e
   checks
   set -e
 
-  echo "Cloning docker images and operators to [$TEMP_RELEASE_FOLDER]"
   tag_repos
   cleanup
 }
