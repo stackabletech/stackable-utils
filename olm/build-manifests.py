48da754ea2b8fc43a6c542949c8056a1321b3152
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2024-04-17 12:49:31 +0200

    wip: patch version in exported manifests

--- olm/build-manifests.py
+++ olm/build-manifests.py
@@ -411,6 +411,14 @@ def generate_helm_templates(args: argparse.Namespace) -> list[dict]:
                         "verbs": ["use"],
                     }
                 )
+            ### Patch the version label
+            if (
+                crv := man["metadata"]["labels"]["app.kubernetes.io/version"]
+            ) != args.release:
+                logging.warning(
+                    f"Version mismatch for '{man['metadata']['name']}'. Replacing '{crv}' with '{args.release}'"
+                )
+                man["metadata"]["labels"]["app.kubernetes.io/version"] = args.release
 
         logging.debug("finish generate_helm_templates")
 
