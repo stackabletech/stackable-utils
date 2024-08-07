72fce5f89ed32d2157d69a3b4dd3cd362425488b
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2024-08-07 17:21:11 +0200

    fix(build-manifests.sh): channels

--- olm/build-manifests.sh
+++ olm/build-manifests.sh
@@ -51,7 +51,7 @@ generate_metadata() {
 ---
 annotations:
   operators.operatorframework.io.bundle.channel.default.v1: "${RELEASE}"
-  operators.operatorframework.io.bundle.channels.v1: "${RELEASE}"
+  operators.operatorframework.io.bundle.channels.v1: "stable,${RELEASE}"
   operators.operatorframework.io.bundle.manifests.v1: manifests/
   operators.operatorframework.io.bundle.mediatype.v1: registry+v1
   operators.operatorframework.io.bundle.metadata.v1: metadata/
