9fee40a3df4989db35976fca7114e547a1e5acf4
Lars Francke <lars.francke@stackable.tech>
2024-10-11 08:32:38 +0200

    Renovate updates (#88)
    * Add more repos
    
    * Add more repos, update renovate
    
    * Update Renovate
    
    remove deprecated config option

--- renovate/run_renovate.sh
+++ renovate/run_renovate.sh
@@ -1,7 +1,7 @@
-# Renovate 37.351 - slim | Updated 2024-05-08
+# Renovate 38.109  | Updated 2024-09-10
 # https://hub.docker.com/r/renovate/renovate/tags
 docker run --rm \
   --volume "./config.js:/usr/src/app/config.js" \
   --env RENOVATE_TOKEN="$GITHUB_TOKEN" \
   --env LOG_LEVEL=debug \
-  renovate/renovate@sha256:3286096674fc3e5d5d3e74698c144113930ffbc4f900ddc9f99d6c81c682f448
+  renovate/renovate@sha256:09510b1f2c32697a83e15c5626b064173a254bc759c75a3d151d680ef93966b2
