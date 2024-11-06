dd4d64b3fd466f2a638073f16f4a233509397809
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2024-11-06 17:22:28 +0100

    test(olm): subscription tolerations (#90)
    * feat(olm): subscription tolerations and env vars
    
    * working version

--- olm/build-bundles.sh
+++ olm/build-bundles.sh
@@ -44,182 +44,197 @@ set -x
 SCRIPT_NAME=$(basename "$0")
 
 parse_inputs() {
-	VERSION=""
-	OPERATOR=""
-	DEPLOY=false
+  VERSION=""
+  OPERATOR=""
+  DEPLOY=false
 
-	while [[ "$#" -gt 0 ]]; do
-		case $1 in
-		-r | --release)
-			VERSION="$2"
-			shift
-			;;
-		-c)
-			OPENSHIFT_ROOT="$2"
-			shift
-			;;
-		-o | --operator)
-			OPERATOR="$2"
-			shift
-			;;
-		-d | --deploy) DEPLOY=true ;;
-		*)
-			echo "Unknown parameter passed: $1"
-			exit 1
-			;;
-		esac
-		shift
-	done
+  while [[ "$#" -gt 0 ]]; do
+    case $1 in
+    -r | --release)
+      VERSION="$2"
+      shift
+      ;;
+    -c)
+      OPENSHIFT_ROOT="$2"
+      shift
+      ;;
+    -o | --operator)
+      OPERATOR="$2"
+      shift
+      ;;
+    -d | --deploy) DEPLOY=true ;;
+    *)
+      echo "Unknown parameter passed: $1"
+      exit 1
+      ;;
+    esac
+    shift
+  done
 }
 
 bundle-clean() {
-	rm -rf "bundle"
-	rm -rf "bundle.Dockerfile"
+  rm -rf "bundle"
+  rm -rf "bundle.Dockerfile"
 }
 
 build-bundle() {
-	opm alpha bundle generate --directory manifests --package "${OPERATOR}-package" --output-dir bundle --channels "stable,$CHANNEL" --default "$CHANNEL"
-	cp metadata/*.yaml bundle/metadata/
-	docker build -t "docker.stackable.tech/sandbox/${OPERATOR}-bundle:${VERSION}" -f bundle.Dockerfile .
-	docker push "docker.stackable.tech/sandbox/${OPERATOR}-bundle:${VERSION}"
-	opm alpha bundle validate --tag "docker.stackable.tech/sandbox/${OPERATOR}-bundle:${VERSION}" --image-builder docker
+  opm alpha bundle generate --directory manifests --package "${OPERATOR}-package" --output-dir bundle --channels "stable,$CHANNEL" --default "$CHANNEL"
+  cp metadata/*.yaml bundle/metadata/
+  docker build -t "docker.stackable.tech/sandbox/${OPERATOR}-bundle:${VERSION}" -f bundle.Dockerfile .
+  docker push "docker.stackable.tech/sandbox/${OPERATOR}-bundle:${VERSION}"
+  opm alpha bundle validate --tag "docker.stackable.tech/sandbox/${OPERATOR}-bundle:${VERSION}" --image-builder docker
 
-	echo "Bundle built successfully!"
+  echo "Bundle built successfully!"
 }
 
 catalog-clean() {
-	if [ -d "catalog" ]; then
-		rm -rf catalog
-	fi
+  if [ -d "catalog" ]; then
+    rm -rf catalog
+  fi
 
-	rm -f catalog.Dockerfile
-	rm -f catalog-source.yaml
-	rm -f subscription.yaml
-	rm -f operator-group.yaml
+  rm -f catalog.Dockerfile
+  rm -f catalog-source.yaml
+  rm -f subscription.yaml
+  rm -f operator-group.yaml
 }
 
 catalog() {
-	mkdir -p catalog
+  mkdir -p catalog
 
-	opm generate dockerfile catalog
+  opm generate dockerfile catalog
 
-	echo "Initiating package: ${OPERATOR}"
-	opm init "stackable-${OPERATOR}-operator" \
-		--default-channel="$CHANNEL" \
-		--output yaml >"catalog/stackable-${OPERATOR}-operator.yaml"
-	##--description="TODO: add description here" \
+  echo "Initiating package: ${OPERATOR}"
+  opm init "stackable-${OPERATOR}-operator" \
+    --default-channel="$CHANNEL" \
+    --output yaml >"catalog/stackable-${OPERATOR}-operator.yaml"
+  ##--description="TODO: add description here" \
 
-	echo "Add operator to package: ${OPERATOR}"
-	{
-		echo "---"
-		echo "schema: olm.channel"
-		echo "package: stackable-${OPERATOR}-operator"
-		echo "name: \"$CHANNEL\""
-		echo "entries:"
-		echo "- name: ${OPERATOR}-operator.v${VERSION}"
-		echo "---"
-		echo "schema: olm.channel"
-		echo "package: stackable-${OPERATOR}-operator"
-		echo "name: stable"
-		echo "entries:"
-		echo "- name: ${OPERATOR}-operator.v${VERSION}"
+  echo "Add operator to package: ${OPERATOR}"
+  {
+    echo "---"
+    echo "schema: olm.channel"
+    echo "package: stackable-${OPERATOR}-operator"
+    echo "name: \"$CHANNEL\""
+    echo "entries:"
+    echo "- name: ${OPERATOR}-operator.v${VERSION}"
+    echo "---"
+    echo "schema: olm.channel"
+    echo "package: stackable-${OPERATOR}-operator"
+    echo "name: stable"
+    echo "entries:"
+    echo "- name: ${OPERATOR}-operator.v${VERSION}"
 
-	} >>"catalog/stackable-${OPERATOR}-operator.yaml"
-	echo "Render operator: ${OPERATOR}"
-	opm render "docker.stackable.tech/sandbox/${OPERATOR}-bundle:${VERSION}" --output=yaml >>"catalog/stackable-${OPERATOR}-operator.yaml"
+  } >>"catalog/stackable-${OPERATOR}-operator.yaml"
+  echo "Render operator: ${OPERATOR}"
+  opm render "docker.stackable.tech/sandbox/${OPERATOR}-bundle:${VERSION}" --output=yaml >>"catalog/stackable-${OPERATOR}-operator.yaml"
 
-	echo "Validating catalog..."
-	opm validate catalog
+  echo "Validating catalog..."
+  opm validate catalog
 
-	echo "Build and push catalog for all ${OPERATOR} operator..."
-	docker build . -f catalog.Dockerfile -t "docker.stackable.tech/sandbox/stackable-${OPERATOR}-catalog:${VERSION}"
-	docker push "docker.stackable.tech/sandbox/stackable-${OPERATOR}-catalog:${VERSION}"
+  echo "Build and push catalog for all ${OPERATOR} operator..."
+  docker build . -f catalog.Dockerfile -t "docker.stackable.tech/sandbox/stackable-${OPERATOR}-catalog:${VERSION}"
+  docker push "docker.stackable.tech/sandbox/stackable-${OPERATOR}-catalog:${VERSION}"
 
-	echo "Generating catalog source..."
-	{
-		echo "---"
-		echo "apiVersion: operators.coreos.com/v1alpha1"
-		echo "kind: CatalogSource"
-		echo "metadata:"
-		echo "  name: stackable-${OPERATOR}-catalog"
-		echo "spec:"
-		echo "  sourceType: grpc"
-		echo "  image: docker.stackable.tech/sandbox/stackable-${OPERATOR}-catalog:${VERSION}"
-		echo "  displayName: Stackable Catalog"
-		echo "  publisher: Stackable GmbH"
-		echo "  updateStrategy:"
-		echo "    registryPoll:"
-		echo "      interval: 10m"
-	} >catalog-source.yaml
+  echo "Generating catalog source..."
+  cat >catalog-source.yaml <<CATALOGSOURCE
+---
+apiVersion: operators.coreos.com/v1alpha1
+kind: CatalogSource
+metadata:
+  name: stackable-${OPERATOR}-catalog
+spec:
+  sourceType: grpc
+  image: docker.stackable.tech/sandbox/stackable-${OPERATOR}-catalog:${VERSION}
+  displayName: Stackable Catalog
+  publisher: Stackable GmbH
+  updateStrategy:
+    registryPoll:
+      interval: 10m
+  grpcPodConfig:
+    tolerations:
+    - key: keep-out
+      value: "yes"
+      operator: Equal
+      effect: NoSchedule
+CATALOGSOURCE
 
-	echo "Generating subscription ..."
-	{
-		echo "---"
-		echo "apiVersion: operators.coreos.com/v1alpha1"
-		echo "kind: Subscription"
-		echo "metadata:"
-		echo "  name: stackable-${OPERATOR}-subscription"
-		echo "spec:"
-		echo "  channel: '$CHANNEL'"
-		echo "  name: stackable-${OPERATOR}-operator" # this is the package name NOT the operator-name
-		echo "  source: stackable-${OPERATOR}-catalog"
-		echo "  sourceNamespace: stackable-operators"
-		echo "  startingCSV: ${OPERATOR}-operator.v${VERSION}"
-	} >subscription.yaml
+  echo "Generating subscription ..."
+  cat >subscription.yaml <<SUBSCRIPTION
+---
+apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  name: stackable-${OPERATOR}-subscription
+spec:
+  channel: '$CHANNEL'
+  name: stackable-${OPERATOR}-operator # this is the package name NOT the operator-name
+  source: stackable-${OPERATOR}-catalog
+  sourceNamespace: stackable-operators
+  startingCSV: ${OPERATOR}-operator.v${VERSION}
+  config:
+    tolerations:
+    - key: keep-out
+      value: "yes"
+      operator: Equal
+      effect: NoSchedule
+    env:
+    - name: SOME_IMPORTANT_FEATURE_FLAG
+      value: "turn-it-on"
+SUBSCRIPTION
 
-	echo "Generating operator group ..."
-	{
-		echo "---"
-		echo "apiVersion: operators.coreos.com/v1"
-		echo "kind: OperatorGroup"
-		echo "metadata:"
-		echo "  name: stackable-operator-group"
-		echo "spec:"
-		echo "  targetNamespaces:"
-		echo "  - stackable-operators"
-	} >operator-group.yaml
+  echo "Generating operator group ..."
+  cat >operator-group.yaml <<OPERATORGROUP
+---
+apiVersion: operators.coreos.com/v1
+kind: OperatorGroup
+metadata:
+  name: stackable-operator-group
+spec:
+  targetNamespaces:
+  - stackable-operators
+OPERATORGROUP
 
-	echo "Catalog, operator group and subscription built (but not deployed) successfully!"
+  echo "Catalog, operator group and subscription built (but not deployed) successfully!"
 }
 
 deploy() {
-	if $DEPLOY; then
-		kubectl describe namespace stackable-operators || kubectl create namespace stackable-operators
-		kubectl apply --namespace stackable-operators \
-			-f catalog-source.yaml \
-			-f subscription.yaml \
-			-f operator-group.yaml
-		echo "Operator deployment done!"
-	else
-		echo "Skip operator deployment!"
-	fi
+  if $DEPLOY; then
+    kubectl describe namespace stackable-operators || kubectl create namespace stackable-operators
+    kubectl apply --namespace stackable-operators \
+      -f catalog-source.yaml \
+      -f subscription.yaml \
+      -f operator-group.yaml
+    echo "Operator deployment done!"
+  else
+    echo "Skip operator deployment!"
+  fi
 }
 
 main() {
-	parse_inputs "$@"
-	if [ -z "${VERSION}" ] || [ -z "${OPENSHIFT_ROOT}" ] || [ -z "${OPERATOR}" ]; then
-		echo "Usage: $SCRIPT_NAME -r <release> -o <operator> -c <path-to-openshift-repo>"
-		exit 1
-	fi
+  parse_inputs "$@"
+  if [ -z "${VERSION}" ] || [ -z "${OPENSHIFT_ROOT}" ] || [ -z "${OPERATOR}" ]; then
+    echo "Usage: $SCRIPT_NAME -r <release> -o <operator> -c <path-to-openshift-repo>"
+    exit 1
+  fi
 
-  CHANNEL="$(echo "$VERSION"|sed 's/\.[^.]*$//')"
+  CHANNEL="$(echo "$VERSION" | sed 's/\.[^.]*$//')"
 
-	if [ "$OPERATOR" == "spark-k8s" ]; then
-		echo "Renaming operator from spark-k8s to spark"
-		OPERATOR="spark"
-	fi
+  if [ "$OPERATOR" == "spark-k8s" ]; then
+    echo "Renaming operator from spark-k8s to spark"
+    OPERATOR="spark"
+  fi
 
-	# this is the same folder that is also used by build-manifests.sh
-	cd "${OPENSHIFT_ROOT}/operators/stackable-${OPERATOR}-operator/${VERSION}"
+  # this is the same folder that is also used by build-manifests.sh
+  cd "${OPENSHIFT_ROOT}/operators/stackable-${OPERATOR}-operator/${VERSION}"
 
-	# clean up any residual files from previous actions
-	bundle-clean
-	build-bundle
+  # clean up any residual files from previous actions
+  bundle-clean
+  build-bundle
 
-	catalog-clean
-	catalog
+  catalog-clean
+  catalog
 
-	deploy
+  deploy
 }
 
 main "$@"
