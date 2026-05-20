4344722275c31013c9c193bf4d2bdc1148f06390
Nick Larsen <nick.larsen@stackable.tech>
2026-05-20 16:31:22 +0200

    chore(release): Add `shellcheck source=common.sh` directive to all scripts
    Co-authored-by: Andrew Kenworthy <andrew.kenworthy@stackable.de>

--- release/merge-release-candidate.sh
+++ release/merge-release-candidate.sh
@@ -6,6 +6,7 @@ set -euo pipefail
 # set -x
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# shellcheck source=common.sh
 source "$SCRIPT_DIR/common.sh"
 
 # TODO: Consider moving validation (validate_tag, validate_what) into parse_inputs
--- release/post-release.sh
+++ release/post-release.sh
@@ -6,6 +6,7 @@ set -euo pipefail
 # set -x
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# shellcheck source=common.sh
 source "$SCRIPT_DIR/common.sh"
 
 REMOTE="origin"
